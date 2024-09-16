70729c0df108650f056c01d5296d5cf5a3a47318
Numele Dvs <adresa@example.com>
2024-09-16 18:17:54 +0300

    Add new page

---
+++ src/pages/FavoritesPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import TopbarContainer from '../containers/TopbarContainer';
+
+function FavoritesPage() {
+  return (
+    <div>
+      <div>Favorites</div>
+
+      <TopbarContainer />
+    </div>
+  );
+}
+
+export default FavoritesPage;
